87469f44761df44fb6b2efc2bf5fcaac18c6aa1d
SATAN-X <diptiranjandash2018@gmail.com>
2022-06-29 10:47:45 +0530

    score board

---
+++ app/src/main/java/com/example/scoreboard/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.scoreboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
